839421cb5a13372dc82db4d4995e9f8ea45e9328
Shubham-1103 <s.sharma3625@gmail.com>
2020-09-13 18:16:07 +0530

    added new features

--- public/script.js
+++ public/script.js
@@ -1,8 +1,19 @@
-const socket = io('/');
-const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.muted = true;
-let myVideoStream;
+// getting the reference to the socket
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+let myVideoStream
+// peer library allow us to run the peer server
+/**
+ * param 1: it is an Id we left it undefined beacuse we want our peer server to take care of Id generation.
+ * param 2: host, port  
+ */  
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '443'
+});
 //gives the ability to get vedio and audio output from chrome
 // it will return promise it can be resolved or not.
 navigator.mediaDevices.getUserMedia({
@@ -11,6 +22,41 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream =>{
     myVideoStream = stream;
     addVedioStream(myVideo, stream);
+    peer.on('call', call=>{
+        call.answer(stream)
+        const vedio = document.createElement('vedio')
+        call.on('stream', userVedioStream=>{
+            addVedioStream(vedio, userVedioStream)
+        })
+    })
+    //listening to the event when user connected 
+    socket.on('user-connected', (userId, stream)=>{
+        setTimeout(()=>connectToNewUser(userId,stream), 3000);
+    })
+
+
+    const  connectToNewUser = (userId, stream) =>{
+        // this will call a user to which we had given a certain ID
+        const call =  peer.call(userId,stream);
+        const vedio = document.createElement('vedio'); 
+        //when user will send us his stream 
+    call.on('stream', userVedioStream=>{
+            addVedioStream(vedio,userVedioStream)
+        })
+    }
+
+    let text = $('input')
+
+    $('html').keydown((e)=>{
+        if(e.which == 13 && text.val().length !=0){
+        socket.emit('message', text.val());
+        text.val('')
+        }
+    }) 
+    socket.on('createMessage', message=>{
+        $('ul').append(`<li class = "message"><b>user</b><br/>${message}</li>`);
+        scrollBottom();
+    })
 })
 const addVedioStream = (video, stream)=> {
     video.srcObject = stream;
@@ -18,12 +64,72 @@ const addVedioStream = (video, stream)=> {
         video.play();
     })
     videoGrid.append(video);
-}
-socket.on('user-connected', ()=>{
-    connectToNewUser();
+} 
+/**
+ * as soon as we actually connect with our peer server and get back an ID 
+ * we actually want to run this code.
+ */
+peer.on('open', id=>{
+    socket.emit('join-room', ROOM_ID, id);
 })
 
-const connectToNewUser = () =>{
-    console.log('new user');
+
+
+const scrollBottom = ()=>{
+    let d = $(".main__chat__window");
+    d.scrollTop(d.prop("scrollHeight"));
 }
-socket.emit('join-room', ROOM_ID)
+
+const muteUnmute = () =>{
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    }
+    else{
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+const setMuteButton = ()=>{
+    const html = `<i class = "fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main__mute__button').innerHTML = html; 
+}
+
+const setUnmuteButton = ()=>{
+    const html = `<i class = "unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `
+    document.querySelector('.main__mute__button').innerHTML = html; 
+}
+
+const playStop = ()=>{
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    }
+    else{
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setPlayVideo = ()=>{
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+    <span> Play Video <span>
+    `
+    document.querySelector('.main__video__button').innerHTML = html;
+}
+
+const setStopVideo = ()=>{
+    const html = `
+    <i class="fas fa-video"></i>
+    <span> Stop Video <span>
+    `
+    document.querySelector('.main__video__button').innerHTML = html;
+}
+
--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const server = require('http').Server(app);
 const {v4:uuidv4} = require('uuid');
 const io = require('socket.io')(server)
+//peer js 
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server,{debug:true}); 
+
 
 // telling where to look for 
 app.use(express.static('public'));
+
+app.use('/peerjs', peerServer); 
 
 //redirection to random uuid
 app.get('/',(req, res)=>{
@@ -15,11 +21,20 @@ app.get('/',(req, res)=>{
 app.get('/:room', (req,res)=>{
     res.render('room',{roomId: req.params.room})
 })
-
+// this gonna run anytime someone connects to our web page
+//admiting a socket here this is the actual socket the user is connecting to.
 io.on('connection', socket=>{
-    socket.on('join-room',(roomId)=>{
+    //setting up the events to listen to
+    //this event is when someone connects to the room
+    socket.on('join-room',(roomId, userId)=>{
+        //we want the current socket to join the room 
         socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected');
+        //brodcasting message to the the room
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.on('message', message=>{
+            io.to(roomId).emit('createMessage', message)
+        })
+
     })
 })
 
